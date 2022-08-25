c0f525f3a3dbcdb6f316a8f4e7ae2bc792c94e03
raselASrock <mdraselm413@gmail.com>
2022-08-26 01:14:02 +0600

    exercise-3-added

---
+++ exercise-3.js
@@ -0,0 +1,12 @@
+const a ={name:`Laptop`, color:`black`, price: 12000};
+const b = {name:`Monitor`, color:`gray`, price: 5000};
+const c = {name:`Mobile`, color:`black`, price: 2500};
+
+const total = a.price + b.price + c.price;
+// console.log(total);
+const laptopPrice = a.price;
+const monitorPrice = b.price;
+const mobilePrice = c.price;
+
+const totalPrice = laptopPrice + monitorPrice + mobilePrice ;
+console.log(totalPrice);
