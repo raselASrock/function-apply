80a75b6b81bc4f1d91a0749d82cafc96e81228b4
raselASrock <mdraselm413@gmail.com>
2022-08-26 01:18:26 +0600

    switch-case-added

---
+++ switch-case.js
@@ -0,0 +1,21 @@
+const grade = 'D';
+
+switch(grade){
+    case 'A+' :
+        console.log('Best Of Luck!!!!');
+    break;
+    case 'A' :
+        console.log('Good Job!!!!');
+    break;
+    case 'B' :
+        console.log('Keep It Up!!!!');
+    break;
+    case 'C' :
+        console.log('Work Hard !!!!!!');
+    break;
+    case 'F' :
+        console.log('Never Give Up');
+    break;
+    default :
+        console.log('Unknown Grade!!!');
+}
