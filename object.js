32a7e496bc59fd29ae2b1f70b11e9fb6901217b1
raselASrock <mdraselm413@gmail.com>
2022-08-26 01:15:45 +0600

    object-added

---
+++ object.js
@@ -0,0 +1,16 @@
+/* Object */
+
+const car = {
+    brand: "Tesla",
+    model: 3,
+    weight: "980 kg",
+    seating: "5 Adults",
+    start() {
+        return  'Car is starting....!!!!';
+    }
+}
+// console.log(car);
+// Dot Notation
+console.log(car.weight);
+// Bracket Notation
+console.log(car['brand']);
